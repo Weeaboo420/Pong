153497d2093d71081d0ab2e5c0928e7504f39e69
Weeaboo420 <rhg2005@hotmail.com>
2020-10-08 16:29:00 +0200

    feat(): Added settings in main menu

--- Pong/Assets/Resources/Scripts/MainMenuManager.cs
+++ Pong/Assets/Resources/Scripts/MainMenuManager.cs
@@ -1,12 +1,29 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private Slider _masterVolumeSlider;
+
     private void Start()
     {
         //Reset time scale
         Time.timeScale = 1f;
+
+        //Get master volume
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            AudioListener.volume = PlayerPrefs.GetFloat("MasterVolume");
+            _masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        }
+    }
+
+    public void OnMasterVolumeChanged(System.Single NewVolume)
+    {
+        PlayerPrefs.SetFloat("MasterVolume", NewVolume);
+        AudioListener.volume = PlayerPrefs.GetFloat("MasterVolume");
     }
 
     public void StartMultiplayer()
