24b73f109e6339a16aa2c53fd4abd1208fd6b045
Weeaboo420 <rhg2005@hotmail.com>
2020-10-08 11:28:20 +0200

    feat(): Added Vsync setting and internal framerate cap

---
+++ Pong/Assets/Resources/Scripts/GraphicsSettings.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicsSettings : MonoBehaviour
+{
+    [SerializeField]
+    private Toggle _vsyncToggle;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 300; //Limit the framerate internally to 300.
+        if(PlayerPrefs.HasKey("VsyncCount"))
+        {
+            QualitySettings.vSyncCount = PlayerPrefs.GetInt("VsyncCount");
+
+            if(QualitySettings.vSyncCount == 1)
+            {
+                _vsyncToggle.isOn = true;
+            } else
+            {
+                _vsyncToggle.isOn = false;
+            }
+        }
+    }
+
+    public void ToggleVsync(bool newVsyncBool)
+    {
+        if(newVsyncBool)
+        {
+            QualitySettings.vSyncCount = 1;
+            
+        } else
+        {
+            QualitySettings.vSyncCount = 0;            
+        }
+
+        PlayerPrefs.SetInt("VsyncCount", QualitySettings.vSyncCount);
+    }
+}
